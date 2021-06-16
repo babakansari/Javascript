9e3f48b7f4938415cc38616b769f63e15c1511d2
Babak Ansari-Jaberi <babakansari@yahoo.com>
2021-06-16 17:52:27 -0400

    Add another permutation example

---
+++ Utilities/Graph/Statistics/Examples/letterCombinations.js
@@ -0,0 +1,50 @@
+/**
+ * N permutaion of elements in two or more arrays:
+ *  
+ */
+var letterCombinations = function(digits) {
+    if (!digits || digits.length === 0) {
+        return [];
+    }
+    
+    const res = [];
+    const mapping = getNumberLetterMap();
+    dfs([], 0);
+
+    function dfs(curList, index) {
+        if (index === digits.length) {
+            res.push(curList.join(""));
+            return;
+        }
+        
+        const curDigit = digits.charAt(index);
+        for (var letter of mapping.get(curDigit)) { 
+            dfs(curList.concat(letter), index + 1);
+        }
+    }   
+
+    function getNumberLetterMap() {
+        const map = [
+            ['2', "abc"], 
+            ['3', "def"], 
+            ['4', "ghi"], 
+            ['5', "jkl"], 
+            ['6', "mno"], 
+            ['7', "pqrs"], 
+            ['8', "tuv"], 
+            ['9', "wxyz"]];
+        return new Map(map);
+    }
+
+    return res;
+};
+
+
+/**
+ *  P(3, 2) * P(2, 2) = 6 * 1 = 6
+ */
+
+let digits = "23"
+// ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+console.log( letterCombinations(digits).toString() );
